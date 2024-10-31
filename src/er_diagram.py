ca88e86f332ac077216a371125590915dcb9574c
Ege Kabasakaloglu <egekabas@mit.edu>
2024-10-30 23:05:03 -0400

    __str__ for er diagram objects

--- src/er_diagram.py
+++ src/er_diagram.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import pandas as pd
 
 class AttributeType(Enum):
     STRING = 1
@@ -10,6 +11,7 @@ class AttributeType(Enum):
     DATETIME = 7
     BINARY = 8
 
+
 class Column:
     def __init__(self, name, type):
         self.name = name
@@ -18,13 +20,27 @@ class Column:
     def __eq__(self, other):
         return self.name == other.name
 
+    def __str__(self):
+        return f"{self.name} ({self.type.name})"
+
 class Table:
-    def __init__(self, name, columns):
+    def __init__(self, name, columns, primary_key=None):
         self.name = name
         self.columns = columns
-    
+
+        if primary_key is not None and primary_key not in columns:
+            raise ValueError("Primary key must be a column in the table")
+        if primary_key is None:
+            primary_key = Column("id", AttributeType.INTEGER)
+            self.columns.append(primary_key)            
+
+        self.primary_key = primary_key
+
     def __eq__(self, other):
         return self.name == other.name
+    
+    def __str__(self):
+        return f"{self.name} ({', '.join([str(column) for column in self.columns])}), pk: {self.primary_key.name}"
 
 class RelationshipType(Enum):
     ONE_TO_ONE = 1
@@ -33,17 +49,16 @@ class RelationshipType(Enum):
     MANY_TO_MANY = 4
 
 class Relationship:
-    def __init__(self, name, from_table, to_table, from_exact, to_exact, type):
+    def __init__(self, name, from_table, to_table, type):
         self.name = name
 
         self.from_table = from_table
-        self.from_exact = from_exact
-
         self.to_table = to_table
-        self.to_exact = to_exact
 
         self.type = type
 
+    def __str__(self):
+        return f"{self.name} ({self.from_table.name} to {self.to_table.name}, {self.type.name})"
 
 class ERDiagram:
     def __init__(self, tables, relationships):
@@ -54,12 +69,18 @@ class ERDiagram:
             if relationship.from_table not in tables or relationship.to_table not in tables:
                 raise ValueError("Invalid relationship")
 
+    def __str__(self):
+        res = "ER DIAGRAM\n"
+        res += "\tTables:\n\t\t"
+        res += "\n\t\t".join([str(table) for table in self.tables])
+        res += "\n\tRelationships:\n\t\t"
+        res += "\n\t\t".join([str(relationship) for relationship in self.relationships])
+        return res
 
 if __name__ == "__main__":
     animal = Table(
         name = "animal",
         columns = [
-            Column("id", AttributeType.INTEGER),
             Column("name", AttributeType.STRING),
             Column("age", AttributeType.INTEGER)
         ]
@@ -67,7 +88,6 @@ if __name__ == "__main__":
     adopter = Table(
         name = "adopter",
         columns = [
-            Column("id", AttributeType.INTEGER),
             Column("name", AttributeType.STRING),
             Column("phone", AttributeType.STRING),
             Column("email", AttributeType.STRING)
@@ -80,10 +100,9 @@ if __name__ == "__main__":
             Relationship(
                 name = "adoption",
                 from_table = animal,
-                from_exact = False,
                 to_table = adopter,
-                to_exact = False,
                 type = RelationshipType.MANY_TO_ONE
             )
         ]
     )
+    print(diagram)
